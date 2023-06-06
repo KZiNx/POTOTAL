ee9405fe7850154a97c00d1a186ada23d8d39fc2
KZiNx <128431103+KZiNx@users.noreply.github.com>
2023-06-06 18:20:04 -0300

    e

--- model/model/carrinho.java
+++ model/model/carrinho.java
@@ -2,7 +2,10 @@ package model;
 public class carrinho {
 
     private String compras;
-
+/**
+ * 
+ * @param compras String
+ */
     public void setcompras(String compras){
         if (compras.matches("[A-Za-z0-9]*") && compras.length() >= 0) {
             this.compras = compras;
